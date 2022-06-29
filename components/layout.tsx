8f0a18d360ca96153aac16cb5f42121f62c692a2
kitazawa <kitadesign@gmail.com>
2022-06-29 20:26:52 +0900

    ページの大枠設定

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,23 +11,25 @@ interface Props {
 const Layout = ({ children, title, description }: Props) => {
   const pageTitle = title || 'ページタイトル'
   return (
-    <div className="wrap">
-      <Head>
-        <title>{pageTitle}</title>
-        <meta name="description" content={description || 'ホームページ概要'} />
-      </Head>
-      <header>
-        <h1>{pageTitle}</h1>
-      </header>
-      <nav>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-        </ul>
-      </nav>
-      <main>{children}</main>
-      <footer>&copy; Next.js Test</footer>
-    </div>
+    <>
+      <div className="wrap">
+        <Head>
+          <title>{pageTitle}</title>
+          <meta name="description" content={description || 'ページ概要'} />
+        </Head>
+        <header>
+          <h1>{pageTitle}</h1>
+        </header>
+        <nav>
+          <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/posts">Posts</Link></li>
+          </ul>
+        </nav>
+        <main>{children}</main>
+        <footer>&copy; Next.js Test</footer>
+      </div>
+    </>
   )
 }
 
